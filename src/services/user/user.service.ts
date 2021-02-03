70babffc7f527920b888525ea9233e67f988231b
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-02-04 00:05:15 +0100

    [UPDATE] user info

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -2,6 +2,7 @@
 
 import {Users} from "../../db/models/users.model";
 import {NextFunction,Response,Request} from "express";
+const jwt = require('jsonwebtoken');
 
 export const userService = {
     getUserByEmailWithToken: async (req:Request, res:Response, next: NextFunction) => {
@@ -9,11 +10,23 @@ export const userService = {
         const {authorization} = req.headers;
         if ( authorization.split(" ")[1] ) {
 
-            let user = await Users.findOne({
-                where:  {
-                    email: req.query.email
-                },
-            });
+            let decoded = jwt.verify(authorization.split(" ")[1],  process.env.JWT_CLIENT_TOKEN_SECRET);
+
+            if ( decoded ) {
+                let user = await Users.findOne({
+                    where:  {
+                        email: decoded.email
+                    },
+                });
+                res.status(200).json({
+                    data: user,
+                    message:" User detail"
+                })
+            } else {
+                res.status(403).json({
+                    message:"token not given"
+                })
+            }
         } else {
             res.status(403).json({
                 message:"token not given"
